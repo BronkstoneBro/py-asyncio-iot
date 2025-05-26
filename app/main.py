a48a210494afbb71294f1e2adf283287b81e6ca0
Бронислав <atlantwindow@gmail.com>
2025-05-26 15:01:33 +0300

    mission completed (update minor fix)

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ async def main() -> None:
     hue_light_id, speaker_id, toilet_id = await asyncio.gather(
         service.register_device(hue_light),
         service.register_device(speaker),
-        service.register_device(toilet)
+        service.register_device(toilet),
     )
 
     # Wake up program
@@ -27,10 +27,14 @@ async def main() -> None:
     # First turn on devices in parallel
     await run_parallel(
         service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
-        service.send_msg(Message(speaker_id, MessageType.SWITCH_ON))
+        service.send_msg(Message(speaker_id, MessageType.SWITCH_ON)),
     )
     # Then play music (must be after speaker is turned on)
-    await service.send_msg(Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"))
+    await service.send_msg(
+        Message(
+            speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"
+        )
+    )
     print("=====END OF WAKE UP PROGRAM======")
 
     # Sleep program
@@ -38,12 +42,12 @@ async def main() -> None:
     # First turn off devices in parallel
     await run_parallel(
         service.send_msg(Message(hue_light_id, MessageType.SWITCH_OFF)),
-        service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF))
+        service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF)),
     )
     # Then handle toilet operations in sequence
     await run_sequence(
         service.send_msg(Message(toilet_id, MessageType.FLUSH)),
-        service.send_msg(Message(toilet_id, MessageType.CLEAN))
+        service.send_msg(Message(toilet_id, MessageType.CLEAN)),
     )
     print("=====END OF SLEEP PROGRAM======")
 
